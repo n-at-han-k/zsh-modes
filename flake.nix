c61f79a0e8e1b75a618fbcb24aa4f6c6a75cf13c
Nathan Kidd <nathankidd@hey.com>
2026-03-12 12:01:06 +0000

    update flake

--- flake.nix
+++ flake.nix
@@ -34,6 +34,75 @@
         };
       });
 
+      nixosModules.default = self.nixosModules.zsh-modes;
+
+      nixosModules.zsh-modes = { config, lib, pkgs, ... }:
+        let
+          cfg = config.programs.zsh.zsh-modes;
+
+          modeOptions = {
+            ruby = {
+              description = "Stateful Ruby REPL (IRB-like)";
+              packages = [ pkgs.ruby ];
+            };
+            kubectl = {
+              description = "Prepend kubectl to all commands";
+              packages = [ pkgs.kubectl ];
+            };
+            docker = {
+              description = "Prepend docker to all commands";
+              packages = [ pkgs.docker-client ];
+            };
+            git = {
+              description = "Prepend git to all commands";
+              packages = [ pkgs.git ];
+            };
+            nix = {
+              description = "Prepend nix to all commands";
+              packages = [ ];
+            };
+          };
+
+          plugin = self.packages.${pkgs.system}.zsh-modes;
+        in
+        {
+          options.programs.zsh.zsh-modes = {
+            enable = lib.mkEnableOption "zsh-modes plugin";
+
+            modes = lib.mkOption {
+              type = lib.types.listOf (lib.types.enum (builtins.attrNames modeOptions));
+              default = builtins.attrNames modeOptions;
+              description = "Which modes to enable (controls runtime dependencies)";
+            };
+
+            extraModes = lib.mkOption {
+              type = lib.types.listOf lib.types.path;
+              default = [ ];
+              description = "Additional mode definition files to load";
+            };
+          };
+
+          config = lib.mkIf cfg.enable {
+            programs.zsh.enable = lib.mkDefault true;
+
+            environment.systemPackages =
+              [ plugin ] ++
+              lib.pipe cfg.modes [
+                (builtins.map (m: modeOptions.${m}.packages))
+                builtins.concatLists
+              ];
+
+            programs.zsh.interactiveShellInit =
+              let
+                extraSourceLines =
+                  builtins.map (f: "source ${f}") cfg.extraModes;
+              in
+              lib.concatStringsSep "\n" ([
+                "source ${plugin}/share/zsh-modes/zsh-modes.plugin.zsh"
+              ] ++ extraSourceLines);
+          };
+        };
+
       homeManagerModules.default = self.homeManagerModules.zsh-modes;
 
       homeManagerModules.zsh-modes = { config, lib, pkgs, ... }:
